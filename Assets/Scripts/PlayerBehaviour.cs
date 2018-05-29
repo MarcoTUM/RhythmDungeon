5c061948d50d25336674df13295830e66c4435bf
JemLink <jana.hoffard@t-online.de>
2018-05-29 21:57:35 +0200

    Player animationen drin und funktionierend

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerBehaviour : MonoBehaviour {
+
+    
+
+    //Variables for animation
+    public Animator animator;
+    public float speed;
+    public float moveOffset;
+
+
 
     private Dictionary<Direction, FieldType> _nextField;
 
@@ -20,20 +30,68 @@ public class PlayerBehaviour : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetKeyDown("down") && _nextField[Direction.Down] != FieldType.Wall) {
-            transform.Translate(Vector3.down * GameModel.Instance.Step);
-        }
-        else if (Input.GetKeyDown("left") && _nextField[Direction.Left] != FieldType.Wall)
+        if (animator.GetBool("Standing"))
         {
-            transform.Translate(Vector3.left * GameModel.Instance.Step);
+            if (Input.GetKeyDown("down") && _nextField[Direction.Down] != FieldType.Wall)
+            {
+                MovePlayer("down");
+                //transform.Translate(Vector3.down * GameModel.Instance.Step);
+            }
+            else if (Input.GetKeyDown("left") && _nextField[Direction.Left] != FieldType.Wall)
+            {
+                MovePlayer("left");
+                //transform.Translate(Vector3.left * GameModel.Instance.Step);
+            }
+            else if (Input.GetKeyDown("up") && _nextField[Direction.Up] != FieldType.Wall)
+            {
+                MovePlayer("up");
+                //transform.Translate(Vector3.up * GameModel.Instance.Step);
+            }
+            else if (Input.GetKeyDown("right") && _nextField[Direction.Right] != FieldType.Wall)
+            {
+                MovePlayer("right");
+                //transform.Translate(Vector3.right * GameModel.Instance.Step);
+            }
         }
-        else if (Input.GetKeyDown("up") && _nextField[Direction.Up] != FieldType.Wall)
-        {
-            transform.Translate(Vector3.up * GameModel.Instance.Step);
-        }
-        else if (Input.GetKeyDown("right") && _nextField[Direction.Right] != FieldType.Wall)
-        {
-            transform.Translate(Vector3.right * GameModel.Instance.Step);
+        
+    }
+
+
+    public void MovePlayer(string direction)
+    {
+        animator.SetBool("Standing", false);
+        switch(direction){
+            case "up":
+                if (_nextField[Direction.Up] != FieldType.Wall)
+                {
+                    animator.SetTrigger("GoUp");
+                    StartCoroutine(MovePlayerTo(Vector3.up));
+                }
+                break;
+            case "down":
+                if(_nextField[Direction.Down] != FieldType.Wall)
+                {
+                    animator.SetTrigger("GoDown");
+                    StartCoroutine(MovePlayerTo(Vector3.down));
+                }
+                break;
+            case "right":
+                if (_nextField[Direction.Right] != FieldType.Wall)
+                {
+                    animator.SetTrigger("GoRight");
+                    StartCoroutine(MovePlayerTo(Vector3.right));
+                }
+                break;
+            case "left":
+                if (_nextField[Direction.Left] != FieldType.Wall)
+                {
+                    animator.SetTrigger("GoLeft");
+                    StartCoroutine(MovePlayerTo(Vector3.left));
+                }
+                break;
+            default:
+                Debug.LogError("Something went wrong in switch case statement in PlayerBehaviour");
+                break;
         }
     }
 
@@ -49,5 +107,20 @@ public class PlayerBehaviour : MonoBehaviour {
         if (startTile != null) {
             Gizmos.DrawCube(startTile.transform.position, new Vector3(startTile.transform.localScale.x, startTile.transform.localScale.y, 0));
         }
+    }
+
+    IEnumerator MovePlayerTo(Vector3 direction)
+    {
+        float tmp = 0;
+        while(tmp < GameModel.Instance.Step)
+        {
+            transform.Translate(direction * moveOffset);
+            tmp += moveOffset;
+            yield return new WaitForSeconds(speed);
+            
+        }
+
+        animator.SetBool("Standing", true);
+
     }
 }
