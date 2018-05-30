1c1673459c6c680b1b8f9a2a25e25c5a0d9ba575
Liou <liou.bao@tum.de>
2018-05-30 11:47:01 +0200

    working

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -20,25 +20,25 @@ public class InputManager : MonoBehaviour {
         {
             if (Input.GetKeyDown("down") && m_playerBehavior._nextField[Direction.Down] != FieldType.Wall)
             {
-                m_playerBehavior.transform.Translate(Vector3.down * GameModel.Instance.Step);
+                m_playerBehavior.MovePlayer("down");
                 // Spawn particle effect
                 Instantiate(m_psHitTheBeat, m_rhythmIdicator.transform.position, Quaternion.identity);
             }
             else if (Input.GetKeyDown("left") && m_playerBehavior._nextField[Direction.Left] != FieldType.Wall)
             {
-                m_playerBehavior.transform.Translate(Vector3.left * GameModel.Instance.Step);
+                m_playerBehavior.MovePlayer("left");
                 // Spawn particle effect
                 Instantiate(m_psHitTheBeat, m_rhythmIdicator.transform.position, Quaternion.identity);
             }
             else if (Input.GetKeyDown("up") && m_playerBehavior._nextField[Direction.Up] != FieldType.Wall)
             {
-                m_playerBehavior.transform.Translate(Vector3.up * GameModel.Instance.Step);
+                m_playerBehavior.MovePlayer("up");
                 // Spawn particle effect
                 Instantiate(m_psHitTheBeat, m_rhythmIdicator.transform.position, Quaternion.identity);
             }
             else if (Input.GetKeyDown("right") && m_playerBehavior._nextField[Direction.Right] != FieldType.Wall)
             {
-                m_playerBehavior.transform.Translate(Vector3.right * GameModel.Instance.Step);
+                m_playerBehavior.MovePlayer("right");
                 // Spawn particle effect
                 Instantiate(m_psHitTheBeat, m_rhythmIdicator.transform.position, Quaternion.identity);
             }
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerBehaviour : MonoBehaviour {
+
+    
+
+    //Variables for animation
+    public Animator animator;
+    public float delay;
+    public float moveOffset;
+
+
 
     public Dictionary<Direction, FieldType> _nextField;
 
@@ -20,23 +30,46 @@ public class PlayerBehaviour : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        /*
-        if (Input.GetKeyDown("down") && _nextField[Direction.Down] != FieldType.Wall) {
-            transform.Translate(Vector3.down * GameModel.Instance.Step);
+              
+    }
+
+
+    public void MovePlayer(string direction)
+    {
+        animator.SetBool("Standing", false);
+        switch(direction){
+            case "up":
+                if (_nextField[Direction.Up] != FieldType.Wall)
+                {
+                    animator.SetTrigger("GoUp");
+                    StartCoroutine(MovePlayerTo(Vector3.up));
+                }
+                break;
+            case "down":
+                if(_nextField[Direction.Down] != FieldType.Wall)
+                {
+                    animator.SetTrigger("GoDown");
+                    StartCoroutine(MovePlayerTo(Vector3.down));
+                }
+                break;
+            case "right":
+                if (_nextField[Direction.Right] != FieldType.Wall)
+                {
+                    animator.SetTrigger("GoRight");
+                    StartCoroutine(MovePlayerTo(Vector3.right));
+                }
+                break;
+            case "left":
+                if (_nextField[Direction.Left] != FieldType.Wall)
+                {
+                    animator.SetTrigger("GoLeft");
+                    StartCoroutine(MovePlayerTo(Vector3.left));
+                }
+                break;
+            default:
+                Debug.LogError("Something went wrong in switch case statement in PlayerBehaviour");
+                break;
         }
-        else if (Input.GetKeyDown("left") && _nextField[Direction.Left] != FieldType.Wall)
-        {
-            transform.Translate(Vector3.left * GameModel.Instance.Step);
-        }
-        else if (Input.GetKeyDown("up") && _nextField[Direction.Up] != FieldType.Wall)
-        {
-            transform.Translate(Vector3.up * GameModel.Instance.Step);
-        }
-        else if (Input.GetKeyDown("right") && _nextField[Direction.Right] != FieldType.Wall)
-        {
-            transform.Translate(Vector3.right * GameModel.Instance.Step);
-        }
-         */
     }
 
     public void setField(Direction dir, FieldType val)
@@ -51,5 +84,20 @@ public class PlayerBehaviour : MonoBehaviour {
         if (startTile != null) {
             Gizmos.DrawCube(startTile.transform.position, new Vector3(startTile.transform.localScale.x, startTile.transform.localScale.y, 0));
         }
+    }
+
+    IEnumerator MovePlayerTo(Vector3 direction)
+    {
+        float tmp = 0;
+        while(tmp < GameModel.Instance.Step)
+        {
+            transform.Translate(direction * moveOffset);
+            tmp += moveOffset;
+            yield return new WaitForSeconds(delay);
+            
+        }
+
+        animator.SetBool("Standing", true);
+
     }
 }
