aa8c169b62eeeaf662e215501d192d79e862fa85
lee17017 <liou@gmx.at>
2018-05-30 11:25:50 +0200

    Revert "Hit Input"
    This reverts commit 76031458adcc7aef3a933f88d09539aba4ba599c.

--- Assets/Scripts/RhythmIndicator.cs
+++ Assets/Scripts/RhythmIndicator.cs
@@ -8,15 +8,12 @@ public class RhythmIndicator : MonoBehaviour
     private IEnumerator m_coroutine;
     private bool m_continueCourutine;
 
-    public enum Status { green, red }
-    public Status status;
     public float m_beatPerSecond;
     public float m_reactionTime;
 
     // Use this for initialization
     void Start()
     {
-        status = Status.red;
         m_continueCourutine = true;
         m_coroutine = WaitOnBeat();
         StartCoroutine(m_coroutine);
@@ -38,11 +35,9 @@ public class RhythmIndicator : MonoBehaviour
         {
             while (m_continueCourutine)
             {
-                status = Status.red;
                 GetComponent<SpriteRenderer>().color = new Color(1, 0, 0);
                 yield return new WaitForSecondsRealtime(m_beatPerSecond);
 
-                status = Status.green;
                 GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
                 yield return new WaitForSecondsRealtime(m_reactionTime);
             }
